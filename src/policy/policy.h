8f902fbf5495e90d36ecb5830dd45d4cea558485
ChrisMartl <ChrisMartl@proton.me>
2023-09-21 17:05:26 +0200

    Policy Rule new style datacarrier

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -148,4 +148,6 @@ static inline int64_t GetVirtualTransactionInputSize(const CTxIn& tx)
     return GetVirtualTransactionInputSize(tx, 0, 0);
 }
 
+uint32_t DatacarrierBytes(const CTransaction& tx, const CCoinsViewCache& view);
+
 #endif // BITCOIN_POLICY_POLICY_H
